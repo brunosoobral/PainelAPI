79c3eac05d7e822eadb9bd13fa20854ff2e4de31
Bruno Sobral <bruno.soobral@gmail.com>
2017-10-01 16:53:58 -0300

    Atualização CORS

--- Services/Global.asax.cs
+++ Services/Global.asax.cs
@@ -17,5 +17,18 @@ namespace Services
             //reconhecer a pasta /Areas 
             AreaRegistration.RegisterAllAreas();
         }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
+        }
     }
 }
